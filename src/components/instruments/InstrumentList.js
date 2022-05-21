635724e7c71f2f536bc9680e6180fc4d87f16ae1
Andre Storti Andrade <a.storti@hotmail.com>
2022-05-21 11:34:41 -0300

    estilizacao pagina instruments

--- src/components/instruments/InstrumentList.js
+++ src/components/instruments/InstrumentList.js
@@ -1,8 +1,53 @@
 import React from "react";
+import '../../styles/instruments.css';
 
 const InstrumentList = () => {
+
+    const handleEdit = () => {
+        console.log("OK")
+    }
+
     return(
-        <h1>Lista de Instrumentos</h1>
+        <div className="container">
+            <div>
+                <h1>Cadastro de Instrumentos</h1>
+            </div>
+            <form>
+                <input type="text" placeholder="Novo Instrumento" />
+                <button className="save-button">Salvar</button>
+                <button className="cancel-button">Cancelar</button>
+            </form>
+
+            <h2>Instrumentos</h2>
+                
+            <div className="list">              
+                <ul>
+                    <li>
+                        Violão
+                        <div>
+                            <input onClick={handleEdit} className="edit" type="submit" value='Editar'/>
+                            <input className="exclude" type="submit" value='Excluir'/>
+                        </div>
+                    </li>
+                    <li>
+                        Teclado
+                        <div>
+                            <input className="edit" type="submit" value='Editar'/>
+                            <input className="exclude" type="submit" value='Excluir'/>
+                        </div>
+                    </li>
+                    <li>
+                        Piano
+                        <div>
+                            <input className="edit" type="submit" value='Editar'/>
+                            <input className="exclude" type="submit" value='Excluir'/>
+                        </div>
+                    </li>
+                    
+                </ul>
+            </div>
+            
+        </div>
     )
 }
 
