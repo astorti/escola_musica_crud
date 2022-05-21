11a67d85211a370887a9d220304596082e1a045b
Andre Storti Andrade <a.storti@hotmail.com>
2022-05-21 17:33:10 -0300

    criação e estilização pagina teachers

--- src/components/instruments/InstrumentList.js
+++ src/components/instruments/InstrumentList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../../styles/instruments.css';
+import '../../index.css'
 
 const InstrumentList = () => {
 
--- src/components/teachers/TeacherList.js
+++ src/components/teachers/TeacherList.js
@@ -1,8 +1,58 @@
 import React from "react";
+import '../../styles/teacher.css';
 
 const TeacherList = () => {
+
+    const handleNewTacher = () => {
+        alert("New Teacher")
+    }
     return(
-        <h1>Professores</h1>
+        <div className="container">
+            
+            <h2>Professores</h2>
+            
+            <button onClick={handleNewTacher}>Novo cadastro</button>
+
+            <div className="list-teacher">  
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Instrumento</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>  
+
+                <tbody>
+                    <tr className="table-body">
+                        <td>Maria Conceição Ferreira Alves Milano</td>
+                        <td>professor1@mail.com</td>
+                        <td>(xx) 1234-5678</td>
+                        <td>Violão</td>
+                        <div className="button-align">
+                            <input className="view" type="submit" value='Consultar'/>
+                            <input className="edit" type="submit" value='Editar'/>
+                            <input className="exclude" type="submit" value='Excluir'/>
+                        </div>
+                    </tr>
+
+                    <tr className="table-body">
+                        <td>Professor 2</td>
+                        <td>professor2@mail.com</td>
+                        <td>(xx) 1234-5678</td>
+                        <td>Piano</td>
+                        <div>
+                            <input className="view" type="submit" value='Consultar'/>
+                            <input className="edit" type="submit" value='Editar'/>
+                            <input className="exclude" type="submit" value='Excluir'/>
+                        </div>
+                    </tr>
+                </tbody>          
+                
+            </div>
+            
+        </div>
     )
 }
 
